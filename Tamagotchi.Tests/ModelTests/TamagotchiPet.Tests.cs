0ab167f4c0170914f297b04b7c17f5ceb4d33b32
Abel Trotter and David Ngo <abel@>
2018-07-05 09:00:41 -0700

    Add getter/setter for food decay/replenish and tests

--- Tamagotchi.Tests/ModelTests/TamagotchiPet.Tests.cs
+++ Tamagotchi.Tests/ModelTests/TamagotchiPet.Tests.cs
@@ -8,6 +8,36 @@ namespace Tamagotchi.Tests
     [TestClass]
     public class TamagotchiPetTest
     {
+        [TestMethod]
+        public void SetGetDecayValue_SetsGetsDecayValue_Int()
+        {
+            TamagotchiPet pet = new TamagotchiPet();
+            pet.SetDecayValue(10);
+            Assert.AreEqual(10, pet.GetDecayValue());
+        }
 
+        [TestMethod]
+        public void SetGetReplenishValue_SetsGetsReplenishValue_Int()
+        {
+            TamagotchiPet pet = new TamagotchiPet();
+            pet.SetReplenishValue(5);
+            Assert.AreEqual(5, pet.GetReplenishValue());
+        }
+
+        [TestMethod]
+        public void FoodDecay_SubtractsFood_Int()
+        {
+            TamagotchiPet pet = new TamagotchiPet();
+            pet.FoodDecay();
+            Assert.AreEqual(90, pet.GetFood());
+        }
+
+        [TestMethod]
+        public void FoodReplenish_AddsFood_Int()
+        {
+            TamagotchiPet pet = new TamagotchiPet();
+            pet.FoodReplenish();
+            Assert.AreEqual(110, pet.GetFood());
+        }
     }
 }
--- Tamagotchi/Models/TamagotchiPet.cs
+++ Tamagotchi/Models/TamagotchiPet.cs
@@ -5,6 +5,54 @@ namespace Tamagotchi.Models
 {
     public class TamagotchiPet
     {
+        private int _food;
+        private int _attention;
+        private int _rest;
+        private int _decayValue = 10;
+        private int _replenishValue = 10;
 
+        private static List<TamagotchiPet> _basket = new List<TamagotchiPet>() {};
+
+        public TamagotchiPet()
+        {
+            _food = 100;
+            _attention = 100;
+            _rest = 100;
+        }
+
+        public void SetDecayValue(int decayValue)
+        {
+            _decayValue = decayValue;
+        }
+
+        public int GetDecayValue()
+        {
+            return _decayValue;
+        }
+
+        public void SetReplenishValue(int replenishValue)
+        {
+            _replenishValue = replenishValue;
+        }
+
+        public int GetReplenishValue()
+        {
+            return _replenishValue;
+        }
+
+        public void FoodDecay()
+        {
+            _food -= this.GetDecayValue();
+        }
+
+        public void FoodReplenish()
+        {
+            _food += this.GetReplenishValue();
+        }
+
+        public int GetFood()
+        {
+            return _food;
+        }
     }
 }
