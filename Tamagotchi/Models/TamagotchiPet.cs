e52b0a88a8dd02ed45c775194c6420402bcf6249
Abel Trotter <a_trotter0@hotmail.com>
2018-07-07 14:49:23 -0700

    Make stat replnish changes, add level property, adjust random values

--- Tamagotchi/Models/TamagotchiPet.cs
+++ Tamagotchi/Models/TamagotchiPet.cs
@@ -11,11 +11,16 @@ namespace Tamagotchi.Models
         private const int _MID_REPLENISH_END = 11;
         private const int _MAX_REPLENISH_START = 10;
         private const int _MAX_REPLENISH_END = 21;
-        private const int _MAX_STAT = 100;
+        private const int _MIN_DECAY_VALUE = 5;
+        private const int _MAX_DECAY_VALUE = 11;
+        private const int _MAX_STAT_VALUE = 100;
+
         private string _name;
+        private int _level = 1;
         private int _food;
         private int _attention;
         private int _rest;
+        private Random _randomNum;
         private int _decayValue;
         private int _replenishValue;
         private int _id;
@@ -27,9 +32,9 @@ namespace Tamagotchi.Models
         {
             this.SetName(name);
             _name = this.GetName();
-            _food = _MAX_STAT;
-            _attention = _MAX_STAT;
-            _rest = _MAX_STAT;
+            _food = _MAX_STAT_VALUE;
+            _attention = _MAX_STAT_VALUE;
+            _rest = _MAX_STAT_VALUE;
             _basket.Add(this);
             _id = _basket.Count;
         }
@@ -49,10 +54,36 @@ namespace Tamagotchi.Models
             return _id;
         }
 
-        public void SetDecayValue()
+        public int GetLevel()
+        {
+            return _level;
+        }
+
+        public void LevelUp()
+        {
+            _level++;
+        }
+
+        public void GenerateRandomSeed()
         {
             Random random = new Random();
-            _decayValue = random.Next(5, 15);
+            _randomNum = random;
+        }
+
+        public void SetRandomSeed(Random random)
+        {
+            _randomNum = random;
+        }
+
+        public Random GetRandomSeed()
+        {
+            return _randomNum;
+        }
+
+        public void SetDecayValue()
+        {
+            this.GenerateRandomSeed();
+            _decayValue = _randomNum.Next(_MIN_DECAY_VALUE, _MAX_DECAY_VALUE);
         }
 
         public int GetDecayValue()
@@ -63,8 +94,8 @@ namespace Tamagotchi.Models
 
         public void SetReplenishValue(int min, int max)
         {
-            Random random = new Random();
-            _replenishValue = random.Next(min, max);
+            this.GenerateRandomSeed();
+            _replenishValue = _randomNum.Next(min, max);
         }
 
         public int GetReplenishValue(int min, int max)
@@ -80,11 +111,11 @@ namespace Tamagotchi.Models
 
         public void FoodReplenish()
         {
-            _food += this.GetReplenishValue(_MIN_REPLENISH_START, _MIN_REPLENISH_END);
+            _food += this.GetReplenishValue(_MID_REPLENISH_START, _MID_REPLENISH_END);
             _food = this.CheckForMax(_food);
-            _attention += this.GetReplenishValue(_MIN_REPLENISH_START, _MID_REPLENISH_END);
+            _attention += this.GetReplenishValue(_MIN_REPLENISH_START, _MIN_REPLENISH_END);
             _attention = this.CheckForMax(_rest);
-            _rest += this.GetReplenishValue(_MIN_REPLENISH_START, _MID_REPLENISH_END);
+            _rest += this.GetReplenishValue(_MIN_REPLENISH_START, _MIN_REPLENISH_END);
             _rest = this.CheckForMax(_rest);
         }
 
@@ -132,9 +163,9 @@ namespace Tamagotchi.Models
 
         public int CheckForMax(int stat)
         {
-            if (stat > _MAX_STAT)
+            if (stat > _MAX_STAT_VALUE)
             {
-                stat = _MAX_STAT;
+                stat = _MAX_STAT_VALUE;
             }
             return stat;
         }
